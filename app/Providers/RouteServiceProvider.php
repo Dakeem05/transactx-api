ec5481fd7e732052d6cc1889a5ed792bbdb1b0f2
Oluwatobi Ishola <ishoshot@gmail.com>
2024-02-20 21:57:27 +0100

    Configure Rate Limit for login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -24,9 +27,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        $this->configureRateLimiting();
 
         $this->routes(function () {
             Route::middleware('api')
@@ -36,5 +37,43 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+    }
+
+
+    /**
+     * Configure the rate limiters for the application.
+     *
+     * @return void
+     */
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+            $correctCredentials = $this->checkUserCredentials($request->input('email'), $request->input('password'));
+
+            return $correctCredentials ?
+                [
+                    Limit::perMinute(3)->by($request->input('email'))->response(function (Request $request) {
+                        return response()->json(['message' => 'Rate limit exceeded. Please try again later!'], 401);
+                    }),
+                ]
+                : [];
+        });
+    }
+
+    private function checkUserCredentials($email, $password)
+    {
+        try {
+            $user = User::where('username', $username)->first();
+            if ($user && Hash::check($password, $user->password)) {
+                return true;
+            }
+            return false;
+        } catch (Exception $e) {
+            return response()->json(['code' => '08', 'message' => 'Error encountered - ' . $e->getMessage()], 429);
+        }
     }
 }
