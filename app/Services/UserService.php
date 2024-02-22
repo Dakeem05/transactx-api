514e6a656a3ec6c6bd596de4a310d541a526e706
Oluwatobi Ishola <ishoshot@gmail.com>
2024-02-22 16:59:27 +0100

    Add laravolt/avatar and generateRefCode

---
+++ app/Services/UserService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class UserService
+{
+    /**
+     * Generates a 6 character code
+     * @return String
+     */
+    public static function generateReferralCode()
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 6; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
