423bd28073bf2f0c656731497101dd7b01fe1c1c
Oluwatobi Ishola <ishoshot@gmail.com>
2024-05-18 15:03:45 +0100

    middleware patch

--- app/Http/Middleware/CheckApplicationCredentials.php
+++ app/Http/Middleware/CheckApplicationCredentials.php
@@ -25,17 +25,17 @@ class CheckApplicationCredentials
 
         // Check if header contains BuildKey
         if (!$buildKey || $buildKey != env('APP_BUILD_KEY')) {
-            return TransactX::response('Invalid BuildKey. Please ensure you are on the current build version.', 401);
+            return TransactX::response(['message' => 'Invalid BuildKey. Please ensure you are on the current build version.'], 401);
         }
 
         // Check if header contains AppID and AppKey
         if (!$appId || !$appKey) {
-            return TransactX::response('AppID and AppKEY are required.', 401);
+            return TransactX::response(['message' => 'AppID and AppKEY are required.'], 401);
         }
 
         // Validate AppID and AppKey for TransactX Mobile
         if ($appId != env('MOBILE_APP_ID') || $appKey != env('MOBILE_APP_KEY')) {
-            return TransactX::response('Invalid AppID or AppKEY.', 401);
+            return TransactX::response(['message' => 'Invalid AppID or AppKEY.'], 401);
         }
 
         return $next($request);
