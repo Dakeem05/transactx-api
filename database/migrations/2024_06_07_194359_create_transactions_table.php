53484a02fd836ab4506d6fa7bb92f679d9f3ea3b
Oluwatobi Ishola <ishoshot@gmail.com>
2024-06-07 22:13:44 +0100

    fix migration

--- database/migrations/2024_06_07_194359_create_transactions_table.php
+++ database/migrations/2024_06_07_194359_create_transactions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
+            // $table->id();
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->nullable()->index()->references('id')->on('users')->onDelete('no action');
             $table->foreignUuid('wallet_id')->nullable()->index()->references('id')->on('wallets')->onDelete('no action');
