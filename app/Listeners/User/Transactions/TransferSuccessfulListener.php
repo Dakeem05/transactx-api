6df6d86706ff212b93914ee34c5b9eba8aa60521
Edidiong Samuel <edidiongsamuel14@@gmail.com>
2025-06-13 05:14:43 +0100

    Debugging transfer webhook 6

--- app/Listeners/User/Transactions/TransferSuccessfulListener.php
+++ app/Listeners/User/Transactions/TransferSuccessfulListener.php
@@ -41,7 +41,7 @@ class TransferSuccessfulListener implements ShouldQueue
             $wallet = $transaction->wallet;
             $user = $transaction->user;
 
-            Log::info("TransferSuccessfulListener.handle() - Processing transfer for user: {$user->id}, transaction: {$transaction->id}");
+            Log::info("TransferSuccessfulListener.handle() - Processing transfer for user: {$user->id}, transaction: {$transaction}");
 
             $recipient_wallet = Wallet::whereHas('virtualBankAccount', function ($query) use ($account_number, $currency) {
                 $query->where([
