2a53c4ebc071579acf5f745e0aaae07d9b3b27c7
Edidiong Samuel <edidiongsamuel14@@gmail.com>
2025-07-17 15:32:53 +0100

    Debugging middleware issue 5

--- app/Http/Middleware/Subscription/Subaccount.php
+++ app/Http/Middleware/Subscription/Subaccount.php
@@ -26,7 +26,7 @@ class Subaccount
         $features = json_decode($user->subscription->model->features);
         $count = User::where('main_account_id', $user->id)->count();
         
-        if ($features->sub_accounts->limit == 0 || $count == $features->sub_accounts->limit) {
+        if ($features->sub_accounts->limit == 0 || $count >= $features->sub_accounts->limit) {
             return TransactX::response(false, 'You are not allowed to proceed due to your subscription plan.', 403);
         }
 
